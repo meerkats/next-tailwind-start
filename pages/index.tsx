079ca97ed20b22e451b934b5f76f3c0d6b448039
John Lombardo <john@programbo.com>
2020-10-28 16:32:04 +0800

    Random change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-blue-500" data-cy="heading">
+    <h1 className="text-green-500" data-cy="heading">
       Hello world
     </h1>
   )
