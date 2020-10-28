79430180ff4c3283ba736eb9750683d8239db888
John Lombardo <john@programbo.com>
2020-10-28 21:41:25 +0800

    Use the standard export syntax

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
 import Document, { DocumentContext } from 'next/document'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
 
     return initialProps
   }
 }
-
-export default MyDocument
