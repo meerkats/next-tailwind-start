a33499fa09d0ba90a21740bc5db3850a65f25887
John Lombardo <john@programbo.com>
2020-10-28 21:45:53 +0800

    Specify explicit language on the HTML tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,21 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
 
     return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
